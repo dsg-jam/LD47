e82e23c67b566d9ab1e6eb616d520b6ffc40b231
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-10-04 22:56:49 +0200

    Update Enemy.gd

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -2,7 +2,7 @@ extends RigidBody2D
 
 const TARGET_GROUP: String = "enemy-target"
 
-export(float) var speed: float = 10.0
+export(float) var accel: float = 10.0
 
 var _target: RigidBody2D
 var _rng := RandomNumberGenerator.new()
@@ -11,11 +11,11 @@ func _physics_process(_delta: float):
 	var my_pos := self.position
 	if self._target:
 		var pos_offset := _target.position - my_pos
-		self.add_central_force(pos_offset * speed)
+		self.applied_force = pos_offset.normalized() * accel
 	else:
 		var x := self._rng.randf_range(-1.0, 1.0)
 		var y := self._rng.randf_range(-1.0, 1.0)
-		self.add_central_force(Vector2(x, y))
+		self.applied_force = Vector2(x, y).normalized() * accel
 
 func _on_Area2D_body_entered(body: Node):
 	if body.is_in_group(TARGET_GROUP):
