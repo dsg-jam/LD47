6973e44c5d02b57def2f2ff98a8b61b9e5a6d3e0
mineiwik <29404520+mineiwik@users.noreply.github.com>
2020-10-05 17:22:52 +0200

    Orbiting around orbiting objects

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -9,12 +9,17 @@ var _target: RigidBody2D
 var orbiting_planet
 var _rng := RandomNumberGenerator.new()
 
+var is_orbiting = false
 
 func _integrate_forces(state: Physics2DDirectBodyState) -> void:
 	var my_pos := self.position
 	if self.orbiting_planet:
+		if is_orbiting:
+			return
 		var gravity = state.total_gravity
-		self.set_linear_velocity(((mass * sqrt((gravity.length() * orbiting_planet.position.distance_to(position)))) * gravity.rotated(TAU/4).normalized()))
+		self.set_linear_velocity(Vector2(0, (TAU*50)/60) * state.step + ((mass * sqrt((gravity.length() * orbiting_planet.position.distance_to(position)))) * gravity.rotated(TAU/4).normalized()))
+
+
 #	elif self._target:
 #		var pos_offset := _target.position - my_pos
 #		self.applied_force = pos_offset.normalized() * accel
